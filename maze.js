85be6348f59d8b15b259cd1fccd212e21628e05e
gorillagunz <jovaunrobinson@Yahoo.com>
2016-10-14 21:06:41 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,7 +1,16 @@
-
 document.addEventListener("DOMContentLoaded", function()
 {
-
-	document.getElementById("boundary1").addEventListener("mouseover", function()
-		{ this.className += " youlose";});
+	var lose=false;
+var boundaries = document.getElementsByClassName("boundary");
+	for(var x =0; x < (boundaries.length-1); x++)
+	{
+		boundaries[x].addEventListener("mouseover",function()
+			{
+				for (var j =0; j < (boundaries.length-1); j++)
+				{
+					lose= true;
+					boundaries[j].setAttribute ("class", "boundary youlose");
+				}
+			});
+	}
 });
