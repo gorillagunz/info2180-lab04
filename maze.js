d2b77a956bf37311f2e9c9a059de5dec05aded51
gorillagunz <jovaunrobinson@Yahoo.com>
2016-10-14 21:03:57 -0500

    Add files via upload

---
+++ maze.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function()
+{
+
+	document.getElementById("boundary1").addEventListener("mouseover", function()
+		{ this.className += " youlose";});
+});
